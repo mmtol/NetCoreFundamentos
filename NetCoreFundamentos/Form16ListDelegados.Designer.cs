c77e019fbdb8c1c015c1958161b5a938cd439801
mmtol <mmigueltolosana@gmail.com>
2025-12-17 13:08:44 +0100

    miercoles pt2

---
+++ NetCoreFundamentos/Form16ListDelegados.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form16ListDelegados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblContador = new Label();
+            txtContador = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            SuspendLayout();
+            // 
+            // lblContador
+            // 
+            lblContador.AutoSize = true;
+            lblContador.Location = new Point(38, 15);
+            lblContador.Name = "lblContador";
+            lblContador.Size = new Size(57, 15);
+            lblContador.TabIndex = 0;
+            lblContador.Text = "Contador";
+            // 
+            // txtContador
+            // 
+            txtContador.Location = new Point(129, 12);
+            txtContador.Name = "txtContador";
+            txtContador.Size = new Size(100, 23);
+            txtContador.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(20, 66);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(101, 65);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(182, 65);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 4;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(20, 95);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 5;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(101, 95);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 6;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(182, 95);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 7;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(20, 124);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 8;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(101, 124);
+            button8.Name = "button8";
+            button8.Size = new Size(75, 23);
+            button8.TabIndex = 9;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // Form16ListDelegados
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(276, 171);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(txtContador);
+            Controls.Add(lblContador);
+            Name = "Form16ListDelegados";
+            Text = "Form16ListDelegados";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblContador;
+        private TextBox txtContador;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+    }
+}
---
+++ NetCoreFundamentos/Form16ListDelegados.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form16ListDelegados : Form
+    {
+        private int contador;
+
+        public Form16ListDelegados()
+        {
+            InitializeComponent();
+
+            this.contador = 0;
+            this.txtContador.Text = this.contador.ToString();
+
+            List<Button> botones = new List<Button>();
+            //botones.Add(button1);
+            //botones.Add(button2);
+            //botones.Add(button3);
+            //botones.Add(button4);
+            //botones.Add(button5);
+            //botones.Add(button6);
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button)
+                {
+                    botones.Add((Button)control);
+                }
+            }
+
+            foreach (Button btn in botones)
+            {
+                btn.Click += PulsarBoton;
+            }
+        }
+
+        private void PulsarBoton(object? sender, EventArgs e)
+        {
+            ++contador;
+            this.txtContador.Text = this.contador.ToString();
+            
+            if (this.contador % 2 == 0)
+            {
+                ((Button)sender).BackColor = Color.LightBlue;
+            }
+            else
+            {
+                ((Button)sender).BackColor = Color.LightGreen;
+            }
+        }
+    }
+}
