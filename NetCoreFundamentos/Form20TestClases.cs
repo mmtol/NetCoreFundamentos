880588e6d027eee31ae5f73338c7fd68e65f5583
mmtol <mmigueltolosana@gmail.com>
2026-01-08 11:04:13 +0100

    jueves pt1

---
+++ NetCoreFundamentos/Form20TestClases.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using ProyectoClases;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form20TestClases : Form
+    {
+        public Form20TestClases()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Pepa";
+            persona.Apellido = "Pepita";
+            persona.Edad = -20;
+            persona.Genero = Generos.Fem;
+            persona.Pais = Paises.Noruega;
+
+            this.lstPersonas.Items.Add(persona.Nombre + " " + persona.Apellido + ", " + persona.Edad + ", " + persona.Genero + ", " + persona.Pais);
+        }
+    }
+}
---
+++ ProyectoClases/Persona.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoClases
+{
+    #region ENUMS
+    public enum Generos { Fem, Masc }
+    public enum Paises {Spain, Italia, Belgica, Noruega}
+    #endregion
+
+    public class Persona
+    {
+        #region ATTR
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public Paises Pais { get; set; }
+
+        private int _Edad;
+        private Generos _Genero;
+        #endregion
+
+        #region PROPS
+        public int Edad
+        {
+            get 
+            { 
+                return _Edad; 
+            }
+            set 
+            { 
+                if (value < 0)
+                {
+                    _Edad = 0;
+                    //throw new Exception("La edad no puede ser negativa");
+                }
+                else
+                {
+                    _Edad = value;
+                }
+            }
+        }
+
+        public Generos Genero
+        {
+            get 
+            { 
+                return _Genero; 
+            }
+            set 
+            { 
+                foreach (Generos genero in Enum.GetValues(typeof(Generos)))
+                {
+                    if (value == genero)
+                    {
+                        _Genero = value;
+                    }
+                    else
+                    {
+                        throw new Exception("Género no válido");
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        public Persona()
+        {
+            
+        }
+
+        #region METODOS
+        public string GetNombreCompleto()
+        {
+            return this.Nombre + " " + this.Apellido;
+        }
+
+        public string GetNombreCompleto(bool reves)
+        {
+            string nombre;
+            if (reves)
+            {
+                nombre = this.Apellido + ", " + this.Nombre;
+            }
+            else
+            {
+                nombre = GetNombreCompleto();
+            }
+
+            return nombre;
+        }
+
+        #endregion
+    }
+}
